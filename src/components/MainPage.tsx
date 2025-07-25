d6d7dd0f64492095bcea953307fd39991d472280
Kshitiz <kshitizs2008@gmail.com>
2025-07-25 05:43:32 +0530

    implements save function and error display

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import NodesPanel from "./NodesPanel";
 import Canvas from "./Canvas";
 import SettingsPanel from "./SettingsPanel";
@@ -8,30 +8,39 @@ import { useReactFlow } from "@xyflow/react";
 const MainPage: React.FC = () => {
     const { selectedNodeId } = useFlowContext();
     const { getNodes, getEdges } = useReactFlow();
+    const [saveStatus, setSaveStatus] = useState<null | "error" | "success">(null);
+    const [saveSuccessMessage, setSaveSuccessMessage] = useState("");
 
     const handleSave = () => {
         const nodes = getNodes();
         const edges = getEdges();
 
-        // Find nodes with no incoming edge (empty target handle)
         const nodesWithNoTarget = nodes.filter(node =>
             !edges.some(edge => edge.target === node.id)
         );
 
         if (nodes.length > 1 && nodesWithNoTarget.length > 1) {
-            alert("Error: More than one node has empty target handles!");
-            return;
+            setSaveSuccessMessage("Cannot Save Flow");
+            setSaveStatus("error");
+        }else{
+            setSaveSuccessMessage("Flow saved successfully!");
+            setSaveStatus("success");
         }
-        alert("Flow saved!");
+        setTimeout(() => {
+            setSaveStatus(null);
+            setSaveSuccessMessage("");
+        }, 3000);
     };
 
     return (
         <div className="flex flex-col h-screen w-screen bg-surface">
             <div className="flex justify-end items-center py-4 bg-container text-on-container font-bold">
                 <div className="flex flex-1 items-center justify-center">
-                    <div className="bg-error text-on-error py-2 px-4 rounded-md">
-                        Cannot Save Flow
-                    </div>
+                    {saveStatus && (
+                        <div className={`text-on-error py-2 px-4 rounded-md ${saveStatus=== "error" ? "bg-error" : "bg-green-300"}`}>
+                            {saveSuccessMessage}
+                        </div>
+                    )}
                 </div>
                 <div className="w-1/4 flex items-center justify-center">
                     <button
